ca1c8a96f321fdb6ca3ea5b71d4356810d0d2f49
Damianko135 <139293484+Damianko135@users.noreply.github.com>
2024-11-28 19:59:55 +0100

    Now just a Theme
    pnpm i -D @skeletonlabs/skeleton@next @skeletonlabs/skeleton-svelte@next
    
    is how it was installed, if you want to remove, uninstall and removed any reference to skeleton-svelte

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,23 @@
-import forms from '@tailwindcss/forms';
 import type { Config } from 'tailwindcss';
 
-export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
+// import * as themes from '@skeletonlabs/skeleton/themes';
+import TandemIT from "./TandemIT";
 
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}', contentPath(import.meta.url, 'svelte')],
 	theme: {
 		extend: {}
 	},
-
-	plugins: [forms]
+	plugins: [
+		skeleton({
+			// NOTE: each theme included will be added to your CSS bundle
+			themes: [
+				// themes.cerberus, 
+				// themes.rose, 
+				// themes.legacy,
+				TandemIT
+			]
+		})
+	]
 } satisfies Config;
