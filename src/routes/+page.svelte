86bc90d8aaadc0a28ebe0e75e942295212fd0479
pepijnweijers <weijers.pepijn@gmail.com>
2024-11-28 23:43:15 +0100

    Added Header

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<div class="min-h-[calc(100vh-4.35rem)] flex items-center">
+    <div class="wrapper lg:grid grid-cols-12">
+        <div class="col-span-7">
+            <h1 class="text-6xl font-grotesk leading-tight font-medium text-secondary-50 mb-4">
+                De partner voor infra- en security services door studenten.
+            </h1>
+            <p class="text-lg max-w-lg">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+            </p>
+            <div class="flex items-center gap-x-3 mt-7">
+                <a href="#contact" class="button button-primary">
+                    Contacteer ons
+                </a>
+                <a href="#contact" class="button button-primary">
+                    Cases
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
