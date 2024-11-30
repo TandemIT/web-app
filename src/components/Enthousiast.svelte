cc1e41a55c8910259531cd934db6602dcb311363
pepijnweijers <weijers.pepijn@gmail.com>
2024-11-30 21:54:54 +0100

    Updated Enthousiast v2

--- src/components/Enthousiast.svelte
+++ src/components/Enthousiast.svelte
@@ -1,58 +1,54 @@
-<div class="flex min-h-[calc(100vh-4.35rem)] flex-col gap-8 bg-white p-10 text-black md:flex-row">
-	<img
-		src="/pattern.png"
-		alt="pattern"
-		class="ml-20 h-auto max-w-full rounded-lg object-cover py-4 md:max-w-[50%]"
-	/>
-	<div class="ml-10 mr-auto flex max-w-prose flex-col py-4">
-		<a href="/about" class="text-sm font-bold text-primary-500 hover:underline">~ Over ons</a>
-		<span class="mb-4 mt-2 text-3xl font-bold"
-			>Een hecht team van enthousiaste HBO-i studenten.</span
-		>
-		<p class="text-base leading-relaxed text-gray-700">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit tempore cum unde laboriosam
-			in, praesentium autem cupiditate atque repudiandae aspernatur. Eaque architecto placeat iusto
-			neque ut iure molestiae labore facere!
-		</p>
-		<ul
-			class="list-style-none mt-4 grid min-h-[50%] list-inside grid-cols-2 items-center justify-center gap-6"
-		>
-			<li class="flex gap-2">
-				<img src="check.svg" alt="check" class="inline-block h-5 w-5" />
-				<div>
-					<span class="text-md font-bold">Flexible</span> <br />
-					Lorem ipsum dolor sit amet consectetur adipisicing elit.
-				</div>
-				<div class="flex gap-x-3">
-					<Check class="text-secondary-600" />
-					<div>
-						<span class="font-semibold">Flexibel</span>
-						<p>Stet clita kasd gubergren, no sea takimata sanctus voluptua</p>
-					</div>
-				</div>
-				<div class="flex gap-x-3">
-					<Check class="text-secondary-600" />
-					<div>
-						<span class="font-semibold">Flexibel</span>
-						<p>Stet clita kasd gubergren, no sea takimata sanctus voluptua</p>
-					</div>
-				</div>
-				<div class="flex gap-x-3">
-					<Check class="text-secondary-600" />
-					<div>
-						<span class="font-semibold">Flexibel</span>
-						<p>Stet clita kasd gubergren, no sea takimata sanctus voluptua</p>
-					</div>
-				</div>
-			</li>
-		</ul>
+<script>
+	import ArrowRight from "@tabler/icons-svelte/icons/arrow-right";
+	import Check from "@tabler/icons-svelte/icons/check";
+</script>
 
-		Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem, numquam assumenda quod
-		nisi quas dolorum laudantium? Quia, nisi natus animi assumenda cupiditate tempora odit,
-		reprehenderit doloribus quam vero ad delectus?
-
-		<a href="/contact" class="mb-4 mt-auto w-fit font-bold text-primary-500 underline">
-			Neem contact op &rarr;
-		</a>
+<div class="bg-[#F8F3ED] text-secondary-800 pt-16 pb-36">
+	<div class="wrapper grid lg:grid-cols-12 gap-10">
+		<div class="relative lg:block hidden lg:col-span-4 pr-7 overflow-hidden">
+			<img class="absolute" src="pattern.png" alt="Pattern banner" width="467" height="961" />
+		</div>
+		<div class="lg:col-span-7">
+			<span class="subtitle !text-secondary-600">Over ons</span>
+			<h2 class="header !text-secondary-900">
+				Een hecht team van enthousiaste HBO-i studenten.
+			</h2>
+			<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+			<div class="grid lg:grid-cols-2 gap-8 my-6">
+				<div class="flex gap-x-3">
+					<Check class="text-secondary-600" />
+					<div>
+						<span class="font-semibold">Flexibel</span>
+						<p>Stet clita kasd gubergren, no sea takimata sanctus voluptua</p>
+					</div>
+				</div>
+				<div class="flex gap-x-3">
+					<Check class="text-secondary-600" />
+					<div>
+						<span class="font-semibold">Flexibel</span>
+						<p>Stet clita kasd gubergren, no sea takimata sanctus voluptua</p>
+					</div>
+				</div>
+				<div class="flex gap-x-3">
+					<Check class="text-secondary-600" />
+					<div>
+						<span class="font-semibold">Flexibel</span>
+						<p>Stet clita kasd gubergren, no sea takimata sanctus voluptua</p>
+					</div>
+				</div>
+				<div class="flex gap-x-3">
+					<Check class="text-secondary-600" />
+					<div>
+						<span class="font-semibold">Flexibel</span>
+						<p>Stet clita kasd gubergren, no sea takimata sanctus voluptua</p>
+					</div>
+				</div>
+			</div>
+			<p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
+			<a href={'/contact'} class="flex items-center gap-x-1 font-semibold underline hover:gap-x-2 duration-300 mt-5">
+				Neem contact op
+				<ArrowRight />
+			</a>
+		</div>
 	</div>
 </div>
