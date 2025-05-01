29f2ee780f6910bf75f3cf8c3a610e5a0af76561
Damianko135 <139293484+Damianko135@users.noreply.github.com>
2025-05-01 21:15:02 +0200

    Allowed previewHosts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-		server: {
-			allowedHosts: true
-		  }
+	server: {
+		allowedHosts: true
+		},
+	preview: {
+		allowedHosts: true
+		}
 });
